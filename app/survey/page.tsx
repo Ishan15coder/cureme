89ab93e93b0fae1c394f76283ad26663f9477940
ishan gupta <gishan750@gmail.com>
2026-03-31 02:45:31 +0530

    final

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -9,13 +9,13 @@ import { useRouter } from "next/navigation";
 import { Check } from "lucide-react";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA0DHyKzIoQpQSVi2KU1AgA7mOrcxMsDiM",
-    authDomain: "cureme-ed6d7.firebaseapp.com",
-    projectId: "cureme-ed6d7",
-    storageBucket: "cureme-ed6d7.firebasestorage.app",
-    messagingSenderId: "495173236710",
-    appId: "1:495173236710:web:6bea8835762bdf70618827",
-    measurementId: "G-DRFWEELWCK"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,13 +7,13 @@ import { Stethoscope, Menu, X, MessageCircle, User as UserIcon, LogOut, Globe, S
 
 // You can move this to lib/firebase.ts in a future refactor
 const firebaseConfig = {
-  apiKey: "AIzaSyA0DHyKzIoQpQSVi2KU1AgA7mOrcxMsDiM",
-  authDomain: "cureme-ed6d7.firebaseapp.com",
-  projectId: "cureme-ed6d7",
-  storageBucket: "cureme-ed6d7.firebasestorage.app",
-  messagingSenderId: "495173236710",
-  appId: "1:495173236710:web:6bea8835762bdf70618827",
-  measurementId: "G-DRFWEELWCK",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
